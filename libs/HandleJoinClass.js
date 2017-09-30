99fa425dda24a52483bed032f8c3d520bbe9e807
Tam Pham <tampham47@live.com>
2017-09-30 15:28:30 +0700

    d

--- libs/HandleJoinClass.js
+++ libs/HandleJoinClass.js
@@ -13,22 +13,19 @@ var debug = DebugM('HandleJoinClass');
 var broadCastMessage = function(payload, server) {
   var message = {
     topic: 'SYSTEM_CLASS_DATA',
-    payload: JSON.stringify({
-      user: payload
-    })
+    payload: JSON.stringify(payload.user)
   };
-  console.log('sessionList', message.topic, message.payload);
   server.publish(message);
 };
 
+/**
+ * @param {*} packet { user: {}, session: '---' }
+ * @param {*} client 
+ * @param {*} server 
+ */
 export default function(packet, client, server) {
   var payloadStr = packet.payload.toString();
   var data = utils.parsePayload(payloadStr);
-  console.log('data', data);
-
-  /**
-   * data is current user
-   */
-
+  
   broadCastMessage(data, server);
 }
