dc32aca4e3150e8355618e6e6b70ea856cfd5d30
Tam Pham <tampham47@live.com>
2017-11-19 08:04:41 +0700

    d

--- libs/index.js
+++ libs/index.js
@@ -10,6 +10,7 @@ import { CronJob } from 'cron';
 import joinSection from './join-section';
 import metting from './metting';
 import subscribe from './subscribe';
+import feedback from './feedback';
 
 var server = new mosca.Server(configMosca);
 
@@ -32,17 +33,27 @@ var job02 = new CronJob({
   start: false,
 });
 
-// init app
+var job03 = new CronJob({
+  cronTime: '00 00 22 * * *', // 22h00 everyday
+  onTick: function () {
+    feedback(server);
+  },
+  start: false,
+});
+
+// init cron jobs
 console.log('STARTED!');
 job01.start();
 job02.start();
+job03.start();
 
+
+// start broker
 server.on('error', function(err){});
 server.on('clientConnected', function(client) {});
 server.on('ready', function(){
   console.log('Mosca server is up and running on port: ' + configMosca.port);
 })
-
 // fired when a message is received
 server.on('published', function(packet, client) {
   switch (packet.topic) {
